d8da44ed5c87d450f2a26924754874fdaf303dcc
Lucas Heck <lucasdealexandri.heck@gmail.com>
2025-11-09 21:57:27 -0300

    changed naming (BR <-> BF) and added mass factors

--- python/SDDM.py
+++ python/SDDM.py
@@ -85,6 +85,24 @@ class SDDM:
             return np.array([[0,0,0],[0,1,0],[0,0,1]])[k, self.mapping(1, multiplet)[1]]
         else: return 0
     
+    # For the two body decay Xi -> Xj + W, the spin averaged |M|^2 has a mass term embedded.
+    # This needs to be taken into consideration for BR and BF calculations.
+    # Masses in GeV
+    def mass_factor(self, m1: float, m2: float):
+        mW = 80.37 # GeV
+        # q . p
+        qp = 1/2 * (m1**2 + m2**2 - mW**2)
+        # k . p
+        kp = 1/2 * (m1**2 - m2**2 + mW**2)
+        
+        return (qp + 3*m1*m2 + 2*kp**2 / mW**2)
+    
+    # p* times mass_factor
+    def phase_space(self, M: float, m1: float):
+        mW = 80.37 # GeV
+        print((M**2 - (m1 + mW)**2)*(M**2 - (m1 - mW)**2))
+        return 1 / (2*M) * np.sqrt((M**2 - (m1 + mW)**2)*(M**2 - (m1 - mW)**2)) * self.mass_factor(M, m1)
+    
     # Related to the element \chi^q_{k1} \chi^{q-1}_{k2} W+
     def Wp_matrix_element(self, charge: int, k1: int, k2: int):
         group_factor = lambda multiplet: np.sqrt(multiplet[0]*(multiplet[0] + 1) - (charge - multiplet[1])*(charge - multiplet[1] - 1))
@@ -101,92 +119,58 @@ class SDDM:
             for multiplet in self.allowed_multiplets(charge)
             )
         
-    # Measures the modified branching ratio from physical particle (q, k1) to another (q - 1, k2) and a W+
+    # Measures the branching fraction from physical particle (q, k1) to another (q - 1, k2) and a W+
     # For the SDDM, q can only be 1.
-    def tilde_BRWp(self, charge: int, k1: int, k2: int):
-        numerator = self.Wp_matrix_element(charge, k1, k2)**2 if self.physical_masses[2+k1] > self.physical_masses[k2] else 0
-        denominator = sum(self.Wp_matrix_element(charge, k1, k)**2 if self.physical_masses[2+k1] > self.physical_masses[k] else 0 for k in range(3))
+    def BFWp(self, charge: int, k1: int, k2: int):
+        m1 = self.physical_masses[2+k1]
+        m2 = self.physical_masses[k2]
+        numerator = self.Wp_matrix_element(charge, k1, k2)**2 * self.phase_space(m1, m2) if m1 > m2 else 0
+        denominator = sum(self.Wp_matrix_element(charge, k1, k)**2 * self.phase_space(m1, self.physical_masses[k]) if m1 > self.physical_masses[k] else 0 for k in range(3))
         if denominator == 0: return 0
         return numerator / denominator
     
-    # Measures the modified branching ratio from physical particle (q, k1) to another (q + 1, k2) and a W-
+    # Measures the branching fraction from physical particle (q, k1) to another (q + 1, k2) and a W-
+    # For the SDDM, q can only be 0.
+    def BFWm(self, charge: int, k1: int, k2: int):
+        m1 = self.physical_masses[k1]
+        m2 = self.physical_masses[2 + k2]
+        numerator = self.Wm_matrix_element(charge, k1, k2)**2 * self.phase_space(m1, m2) if m1 > m2 else 0
+        denominator = sum(self.Wm_matrix_element(charge, k1, k)**2 * self.phase_space(m1, self.physical_masses[2+k]) if m1 > self.physical_masses[2+k] else 0 for k in range(1,3))
+        if denominator == 0: return 0
+        return numerator / denominator
+        
+    # Measures the modified branching fraction from physical particle (q, k1) to another (q - 1, k2) and a W+
     # For the SDDM, q can only be 1.
-    def tilde_BRWm(self, charge: int, k1: int, k2: int):
-        numerator = self.Wm_matrix_element(charge, k1, k2)**2
-        kinematically_possible = int(self.physical_masses[k1] > self.physical_masses[2 + k2])
-        numerator *= kinematically_possible
-        denominator = sum(self.Wm_matrix_element(charge, k1, k)**2 if self.physical_masses[k1] > self.physical_masses[2+k] else 0 for k in range(1,3))
+    def tilde_BFWp(self, charge: int, k1: int, k2: int):
+        m1 = self.physical_masses[2+k1]
+        m2 = self.physical_masses[k2]
+        numerator = self.Wp_matrix_element(charge, k1, k2)**2 * self.mass_factor(m1, m2) if m1 > m2 else 0
+        denominator = sum(self.Wp_matrix_element(charge, k1, k)**2 * self.mass_factor(m1, self.physical_masses[k]) if m1 > self.physical_masses[k] else 0 for k in range(3))
+        if denominator == 0: return 0
+        return numerator / denominator
+    
+    # Measures the modified branching fraction from physical particle (q, k1) to another (q + 1, k2) and a W-
+    # For the SDDM, q can only be 0.
+    def tilde_BFWm(self, charge: int, k1: int, k2: int):
+        m1 = self.physical_masses[k1]
+        m2 = self.physical_masses[2 + k2]
+        numerator = self.Wm_matrix_element(charge, k1, k2)**2 * self.mass_factor(m1, m2) if m1 > m2 else 0
+        denominator = sum(self.Wm_matrix_element(charge, k1, k)**2 * self.mass_factor(m1, self.physical_masses[2+k]) if m1 > self.physical_masses[2+k] else 0 for k in range(1,3))
         if denominator == 0: return 0
         return numerator / denominator
     
     @property
-    def tilde_BRW(self):
-        return np.array([[self.tilde_BRWp(1, k2, k1) + self.tilde_BRWm(0, k1, k2) for k1 in range(3)] for k2 in range(1,3)])
+    def BFW(self):
+        return np.array([[self.BFWp(1, k2, k1) + self.BFWm(0, k1, k2) for k1 in range(3)] for k2 in range(1,3)])
     
-    # Metric for determining probability of \chi^{q}_{k1} being on top of \chi^{q-1}_{k2}
-    # in an SU(2) multiplet
-    def upstairs_metric(self, charge: int, k1: int, k2: int):
-        neutral_mass = self.physical_masses[k2]
-        charged_mass = self.physical_masses[2 + k1]
-        delta_m = min(neutral_mass / charged_mass, charged_mass / neutral_mass)
-        return np.sqrt(self.tilde_BRWp(charge, k1, k2) * delta_m)
-    
-    # Metric for determining probability of \chi^{q}_{k1} being below of \chi^{q+1}_{k2}
-    # in an SU(2) multiplet
-    def downstairs_metric(self, charge: int, k1: int, k2: int):
-        neutral_mass = self.physical_masses[k1]
-        charged_mass = self.physical_masses[2 + k2]
-        delta_m = min(neutral_mass / charged_mass, charged_mass / neutral_mass)
-        return np.sqrt(self.tilde_BRWm(charge, k1, k2) * delta_m)
-    
-    def multiplet_metric(self, particle1: List[tuple], particle2: List[tuple]):
-        particles = [particle1, particle2]
-        
-        assert any(particle[0] == 0 for particle in particles), "No neutral particle found"
-        assert any(particle[0] == 1 for particle in particles), "No charged particle found"
-        
-        neutral_particle = particle1 if particle1[0] == 0 else particle2
-        charged_particle = particle1 if particle1[0] == 1 else particle2
-        
-        return self.upstairs_metric(1,neutral_particle[1],charged_particle[1]) + self.downstairs_metric(0,neutral_particle[1],charged_particle[1])
-    
-    def cluster_doublets(self):
-        partners = []
-        neutral_particles = self.physical_particles[:3]
-        charged_particles = self.physical_particles[3:]
-        
-        for particle in charged_particles:
-            distances = [self.multiplet_metric(particle, neutral_particle) for neutral_particle in neutral_particles]
-            candidate = np.argmax(distances)
-            partners.append((particle, neutral_particles[candidate]))    
-            
-        return partners
+    @property
+    def tilde_BFW(self):
+        return np.array([[self.tilde_BFWp(1, k2, k1) + self.tilde_BFWm(0, k1, k2) for k1 in range(3)] for k2 in range(1,3)])
         
     
 if __name__ == "__main__":
     
     sddm = SDDM()
-
-    # print(sddm.allowed_multiplets(1))
-    # print(sddm.mapping(0,(0,0,1)))
-    # print(f"ms = {sddm.ms}, mu = {sddm.mu}, y1 = {sddm.y1}, y2 = {sddm.y2}")
     print(sddm.physical_masses)
-    # print(sddm.change_of_basis_neutral())
-    # print(sddm.change_of_basis(0,0,(0,0,1)))
-    # print("k1 refers to charge = 1, k2 to charge = 0")
-    # print(np.array([[f"(1, {k1}) -> (0, {k2})" for k1 in range(1, 3)] for k2 in range(3)]))
-    # # print(np.array([[f"{sddm.physical_masses[k1]},{sddm.physical_masses[2+k2]}" for k1 in range(1, 3)] for k2 in range(3)]))
-    # print(np.array([[sddm.tilde_BRWp(1, k1, k2) for k1 in range(1,3)] for k2 in range(3)]))
-    # print("k1 refers to charge = 0, k2 to charge = 1")
-    # print(np.array([[f"(0, {k1}) -> (1, {k2})" for k1 in range(3)] for k2 in range(1,3)]))
-    # print(np.array([[sddm.tilde_BRWm(0, k1, k2) for k1 in range(3)] for k2 in range(1,3)]))
-    # print(np.array([[sddm.tilde_BRWp(1, k2, k1) for k1 in range(3)] for k2 in range(1,3)]))
-    print(np.array([[sddm.tilde_BRWp(1, k2, k1) + sddm.tilde_BRWm(0, k1, k2) for k1 in range(3)] for k2 in range(1,3)]))
-    # print(np.array([[sddm.upstairs_metric(1, k2, k1) + sddm.downstairs_metric(0, k1, k2) for k1 in range(3)] for k2 in range(1,3)]))
-    # print("upstairs metric")
-    # print(np.array([[sddm.upstairs_metric(1, k1, k2) for k1 in range(3)] for k2 in range(3)]))
-    # print("downstairs metric")
-    # print(np.array([[sddm.downstairs_metric(0, k2, k1) for k1 in range(3)] for k2 in range(3)]))
-    # print("combined metric")
-    # print(np.array([[sddm.upstairs_metric(1, k1, k2) + sddm.downstairs_metric(0, k2, k1) for k1 in range(3)] for k2 in range(3)]))
-    # print(sddm.cluster_doublets())
+    print(sddm.BFW)
+    print(sddm.tilde_BFW)
